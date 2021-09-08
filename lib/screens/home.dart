c2662d2b3af7bb5934b62db4374b0a6e906c98e3
nvalfre <kokevalfre@gmail.com>
2021-09-08 18:58:55 -0300

    refactor

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -73,48 +73,44 @@ class _HomeState extends State<Home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: 190.0,
-                width: double.infinity,
-                child: Carousel(
-                  boxFit: BoxFit.fill,
-                  autoplay: true,
-                  animationCurve: Curves.fastOutSlowIn,
-                  animationDuration: Duration(milliseconds: 1000),
-                  dotSize: 5.0,
-                  dotIncreasedColor: Colors.purple,
-                  dotBgColor: Colors.black.withOpacity(0.2),
-                  dotPosition: DotPosition.bottomCenter,
-                  showIndicator: true,
-                  indicatorBgPadding: 5.0,
-                  images: [
-                    ExactAssetImage(_carouselImages[0]),
-                    ExactAssetImage(_carouselImages[1]),
-                    ExactAssetImage(_carouselImages[2]),
-                    ExactAssetImage(_carouselImages[3]),
-                  ],
-                ),
+              buildLandingCarousel(),
+              buildPopularProducts(context, productsData),
+              buildCategories(),
+              buildBrands(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildLandingCarousel() {
+    return Container(
+              height: 190.0,
+              width: double.infinity,
+              child: Carousel(
+                boxFit: BoxFit.fill,
+                autoplay: true,
+                animationCurve: Curves.fastOutSlowIn,
+                animationDuration: Duration(milliseconds: 1000),
+                dotSize: 5.0,
+                dotIncreasedColor: Colors.purple,
+                dotBgColor: Colors.black.withOpacity(0.2),
+                dotPosition: DotPosition.bottomCenter,
+                showIndicator: true,
+                indicatorBgPadding: 5.0,
+                images: [
+                  ExactAssetImage(_carouselImages[0]),
+                  ExactAssetImage(_carouselImages[1]),
+                  ExactAssetImage(_carouselImages[2]),
+                  ExactAssetImage(_carouselImages[3]),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Categorías',
-                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 180,
-                child: ListView.builder(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext ctx, int index) {
-                    return CategoryWidget(
-                      index: index,
-                    );
-                  },
-                ),
-              ),
+            );
+  }
+
+  Column buildBrands(BuildContext context) {
+    return Column(children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
@@ -122,7 +118,7 @@ class _HomeState extends State<Home> {
                     Text(
                       'Marcas populares',
                       style:
-                          TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                      TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
                     ),
                     Spacer(),
                     FlatButton(
@@ -174,62 +170,87 @@ class _HomeState extends State<Home> {
                     );
                   },
                 ),
-              ),
+              )
+            ],);
+  }
+
+  Column buildCategories() {
+    return Column(children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'Productos populares',
-                      style:
-                          TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
-                    ),
-                    Spacer(),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(Marketplace.routeName,
-                            arguments: 'popular');
-                      },
-                      child: Text(
-                        'Ver más',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            fontSize: 15,
-                            color: Colors.red),
-                      ),
-                    )
-                  ],
+                child: Text(
+                  'Categorías',
+                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
                 ),
               ),
+              Container(
+                width: double.infinity,
+                height: 180,
+                child: ListView.builder(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return CategoryWidget(
+                      index: index,
+                    );
+                  },
+                ),
+              )
+            ],);
+  }
+
+  Column buildPopularProducts(BuildContext context, Products productsData) {
+    return Column(children: [Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Productos populares',
+                    style:
+                    TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                  ),
+                  Spacer(),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(Marketplace.routeName,
+                          arguments: 'popular');
+                    },
+                    child: Text(
+                      'Ver más',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 15,
+                          color: Colors.red),
+                    ),
+                  )
+                ],
+              ),
+            ),
               FutureBuilder(
-                  future: productsData.fetchProductsList(),
-                  initialData: productsData.products,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Product>> snapshot) {
-                    return snapshot.hasData
-                        ? Container(
-                            width: double.infinity,
-                            height: 285,
-                            margin: EdgeInsets.symmetric(horizontal: 3),
-                            child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: productsData.popularProducts.length,
-                                itemBuilder: (BuildContext ctx, int index) {
-                                  return ChangeNotifierProvider.value(
-                                    value: productsData.popularProducts[index],
-                                    child: PopularProducts(),
-                                  );
-                                }),
-                          )
-                        : CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                Theme.of(context).primaryColor),
+                future: productsData.fetchProductsList(),
+                initialData: productsData.products,
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Product>> snapshot) {
+                  return snapshot.hasData
+                      ? Container(
+                    width: double.infinity,
+                    height: 285,
+                    margin: EdgeInsets.symmetric(horizontal: 3),
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: productsData.popularProducts.length,
+                        itemBuilder: (BuildContext ctx, int index) {
+                          return ChangeNotifierProvider.value(
+                            value: productsData.popularProducts[index],
+                            child: PopularProducts(),
                           );
-                  })
-            ],
-          ),
-        ),
-      ),
-    );
+                        }),
+                  )
+                      : CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).primaryColor),
+                  );
+                },
+              ),],);
   }
 }
